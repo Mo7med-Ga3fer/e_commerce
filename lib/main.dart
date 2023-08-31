9824603522dc8bc0781af2652111641b0d8e648d
Mo7medGa3fer <Mo7medGa3fer@gmail.com>
2023-08-31 22:44:37 +0300

    connect project with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:e_commerce/feature/auth/presentation/views/splash_view.dart';
+import 'package:e_commerce/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-void main() {
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     statusBarColor: Colors.transparent,
     statusBarIconBrightness: Brightness.dark
   ));
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
   runApp(const Ecommerce());
 }
 
